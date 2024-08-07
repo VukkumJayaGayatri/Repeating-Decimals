290fb81c161c647893aa30507aa430c724ff91b1
Vukkum Jaya Gayatri <99681807+VukkumJayaGayatri@users.noreply.github.com>
2024-08-07 23:24:46 +0530

    Update_2 repeating_decimal.py

--- repeating_decimal.py
+++ repeating_decimal.py
@@ -1,16 +1,9 @@
-def repeating_decmials(x,y):
-    if not (x, int) or not (y, int):
 def repeating_decmials(numerator,denominator):
     if not (numerator, int) or not (denominator, int):
         raise ValueError("num and den must be integers")
-    if y == 0:
     if denominator == 0:
         raise ValueError("den cannot be zero")
     result = ""
-x=int(input("enter a numerator:"))
-y=int(input("enter a denominator:"))
-res=x/y
-print(res)
     remainder = numerator % denominator
     quotient = numerator // denominator
     result += str(quotient)
@@ -22,8 +15,16 @@ print(res)
         return result
     result += "."
     remainders = {}
+    while remainder != 0:
+        if remainder in remainders:
+            result = result[: remainders[remainder]]+result[remainders[remainder] :]
+            return result
+        remainders[remainder] = len(result)
+        remainder *= 10
+        quotient = remainder // denominator
+        result += str(quotient)
+        remainder %= denominator
+    return result
 try:
     numerator = int(input("enter a numerator:"))
     denominator = int(input("enter a denominator:"))
-    res = numerator / denominator
-    print(res)
